fd8ddffd975ad41d703d430e3bf62096a6bac00d
M00R3D <jobmurdan@hotmail.com>
2026-03-15 19:10:02 -0700

    usuarios only for admins in sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,9 +44,9 @@
       <a class="nav-item" href="/reservaciones">Reservaciones</a>
       <a class="nav-item" href="/propiedades">Propiedades</a>
       <a class="nav-item" href="/notificaciones">Notificaciones</a>
-      <a class="nav-item" href="/users">Usuarios</a>
       <a class="nav-item" href="{{ route('tarjetas.index') }}">Tarjetas</a>
       @if($isAdmin)
+        <a class="nav-item" href="/users">Usuarios</a>
         <a class="nav-item" href="{{ route('admin.logs') }}">Logs</a>
         <a class="nav-item" href="{{ route('images.index') }}">Imágenes</a>
         <a class="nav-item" href="{{ route('pagos.index') }}">Pagos</a>
