5dc52955255f3b7c012856457a52b711a8181bc8
M00R3D <jobmurdan@hotmail.com>
2026-04-06 21:53:46 -0700

    bootstrap app and trustProxies fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\App\Http\Middleware\TrustProxies::class);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
